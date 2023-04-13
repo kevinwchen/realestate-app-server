f038e774ed078ebe72decf17777c3ad10198211f
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2023-04-13 15:22:43 +1000

    Add base and config files

---
+++ index.js
@@ -0,0 +1,20 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import express from "express";
+const app = express();
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+app.use(express.json());
+
+import propertiesRouter from "./routes/properties.js";
+app.use("/properties", propertiesRouter);
